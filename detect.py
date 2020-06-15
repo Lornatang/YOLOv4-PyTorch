40701436fbb2d6b31e12f1f70078d12285536b30
Lornatang <liuchangyu1111@gmail.com>
2020-06-15 23:00:39 +0800

    Change to a new detection method

--- detect.py
+++ detect.py
@@ -22,43 +22,52 @@ import cv2
 import torch
 import torch.backends.cudnn as cudnn
 
-from yolov4_pytorch import LoadImages
-from yolov4_pytorch import LoadStreams
-from yolov4_pytorch import apply_classifier
-from yolov4_pytorch import check_img_size
-from yolov4_pytorch import load_classifier
-from yolov4_pytorch import non_max_suppression
-from yolov4_pytorch import plot_one_box
-from yolov4_pytorch import scale_coords
-from yolov4_pytorch import select_device
-from yolov4_pytorch import time_synchronized
-from yolov4_pytorch import xyxy2xywh
+from yolov4_pytorch.data import LoadImages
+from yolov4_pytorch.data import LoadStreams
+from yolov4_pytorch.data import check_image_size
+from yolov4_pytorch.model import YOLO
+from yolov4_pytorch.model import apply_classifier
+from yolov4_pytorch.model import load_classifier
+from yolov4_pytorch.utils import non_max_suppression
+from yolov4_pytorch.utils import plot_one_box
+from yolov4_pytorch.utils import scale_coords
+from yolov4_pytorch.utils import select_device
+from yolov4_pytorch.utils import time_synchronized
+from yolov4_pytorch.utils import xyxy2xywh
 
 
-def detect(save_img=False):
-    out, source, weights, half, view_img, save_txt, imgsz = \
-        opt.output, opt.source, opt.weights, opt.half, opt.view_img, opt.save_txt, opt.img_size
+def detect(save_image=False):
+    out = args.output
+    source = args.source
+    weights = args.weights
+    half = args.half
+    view_image = args.view_image
+    save_txt = args.save_txt
+    image_size = args.image_size
+
     webcam = source == "0" or source.startswith("rtsp") or source.startswith("http") or source.endswith(".txt")
 
     # Initialize
-    device = select_device(opt.device)
+    device = select_device(args.device)
     if os.path.exists(out):
         shutil.rmtree(out)  # delete output folder
     os.makedirs(out)  # make new output folder
 
-    # Load model
-    model = torch.load(weights, map_location=device)["model"]
-    # torch.save(torch.load(weights, map_location=device), weights)  # update model if SourceChangeWarning
-    # model.fuse()
+    # Initialize model
+    model = YOLO(args.config_file, image_size)
+
+    # Load weight
+    model.load_state_dict(torch.load(weights, map_location=device)["state_dict"])
     model.to(device).eval()
 
     # Second-stage classifier
     classify = False
-    modelc = None
+    model_classify = None
     if classify:
-        modelc = load_classifier(name="resnet101", classes=2)  # initialize
-        modelc.load_state_dict(torch.load("weights/resnet101.pth", map_location=device)["model"])  # load weights
-        modelc.to(device).eval()
+        model_classify = load_classifier(name="resnet101", classes=2)  # initialize
+        model_classify.load_state_dict(
+            torch.load("weights/resnet101.pth", map_location=device)["model"])  # load weights
+        model_classify.to(device).eval()
 
     # Half precision
     half = half and device.type != "cpu"  # half precision only supported on CUDA
@@ -66,60 +75,60 @@ def detect(save_img=False):
         model.half()
 
     # Set Dataloader
-    vid_path, vid_writer = None, None
+    video_path, video_writer = None, None
     if webcam:
-        view_img = True
+        view_image = True
         cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, image_size=imgsz)
+        dataset = LoadStreams(source, image_size=image_size)
     else:
-        save_img = True
-        dataset = LoadImages(source, image_size=imgsz)
+        save_image = True
+        dataset = LoadImages(source, image_size=image_size)
 
     # Get names and colors
     names = model.names if hasattr(model, "names") else model.modules.names
     colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
 
     # Run inference
-    t0 = time.time()
-    img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
-    _ = model(img.half() if half else img.float()) if device.type != "cpu" else None  # run once
-    for path, img, im0s, vid_cap in dataset:
-        img = torch.from_numpy(img).to(device)
-        img = img.half() if half else img.float()  # uint8 to fp16/32
-        img /= 255.0  # 0 - 255 to 0.0 - 1.0
-        if img.ndimension() == 3:
-            img = img.unsqueeze(0)
+    start_time = time.time()
+    image = torch.zeros((1, 3, image_size, image_size), device=device)  # init img
+    _ = model(image.half() if half else image.float()) if device.type != "cpu" else None  # run once
+    for path, image, raw_images, video_cap in dataset:
+        image = torch.from_numpy(image).to(device)
+        image = image.half() if half else image.float()  # uint8 to fp16/32
+        image /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if image.ndimension() == 3:
+            image = image.unsqueeze(0)
 
         # Inference
         t1 = time_synchronized()
-        pred = model(img, augment=opt.augment)[0]
+        predict = model(image, augment=args.augment)[0]
 
         # to float
         if half:
-            pred = pred.float()
+            predict = predict.float()
 
         # Apply NMS
-        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres,
-                                   fast=True, classes=opt.classes, agnostic=opt.agnostic_nms)
+        predict = non_max_suppression(predict, args.confidence_threshold, args.iou_threshold,
+                                      fast=True, classes=args.classes, agnostic=args.agnostic_nms)
         t2 = time_synchronized()
 
         # Apply Classifier
         if classify:
-            pred = apply_classifier(pred, modelc, img, im0s)
+            predict = apply_classifier(predict, model_classify, image, raw_images)
 
         # Process detections
-        for i, det in enumerate(pred):  # detections per image
+        for i, det in enumerate(predict):  # detections per image
             if webcam:  # batch_size >= 1
-                p, s, im0 = path[i], "%g: " % i, im0s[i].copy()
+                p, s, raw_image = path[i], f"{i}: ", raw_images[i].copy()
             else:
-                p, s, im0 = path, "", im0s
+                p, s, raw_image = path, "", raw_images
 
             save_path = str(Path(out) / Path(p).name)
-            s += "%gx%g " % img.shape[2:]  # print string
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  #  normalization gain whwh
+            s += f"{image.shape[2]}x{image.shape[3]} "  # print string
+            gn = torch.tensor(raw_image.shape)[[1, 0, 1, 0]]  #  normalization gain whwh
             if det is not None and len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+                # Rescale boxes from img_size to raw_image size
+                det[:, :4] = scale_coords(image.shape[2:], det[:, :4], raw_image.shape).round()
 
                 # Print results
                 for c in det[:, -1].unique():
@@ -133,60 +142,75 @@ def detect(save_img=False):
                         with open(save_path[:save_path.rfind(".")] + ".txt", "a") as file:
                             file.write(("%g " * 5 + "\n") % (cls, *xywh))  # label format
 
-                    if save_img or view_img:  # Add bbox to image
-                        label = "%s %.2f" % (names[int(cls)], conf)
-                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+                    if save_image or view_image:  # Add bbox to image
+                        label = f"{names[int(cls)]} {conf:.2f}"
+                        plot_one_box(xyxy, raw_image, label=label, color=colors[int(cls)], line_thickness=3)
 
             # Print time (inference + NMS)
-            print("%sDone. (%.3fs)" % (s, t2 - t1))
+            print(f"{s}Done. ({t2 - t1:.3f}s)")
 
             # Stream results
-            if view_img:
-                cv2.imshow(p, im0)
+            if view_image:
+                cv2.imshow(p, raw_image)
                 if cv2.waitKey(1) == ord("q"):  # q to quit
                     raise StopIteration
 
             # Save results (image with detections)
-            if save_img:
+            if save_image:
                 if dataset.mode == "images":
-                    cv2.imwrite(save_path, im0)
+                    cv2.imwrite(save_path, raw_image)
                 else:
-                    if vid_path != save_path:  # new video
-                        vid_path = save_path
-                        if isinstance(vid_writer, cv2.VideoWriter):
-                            vid_writer.release()  # release previous video writer
+                    if video_path != save_path:  # new video
+                        video_path = save_path
+                        if isinstance(video_writer, cv2.VideoWriter):
+                            video_writer.release()  # release previous video writer
 
-                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*opt.fourcc), fps, (w, h))
-                    vid_writer.write(im0)
+                        fps = video_cap.get(cv2.CAP_PROP_FPS)
+                        w = int(video_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                        h = int(video_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                        video_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*args.fourcc), fps, (w, h))
+                    video_writer.write(raw_image)
 
-    if save_txt or save_img:
-        print("Results saved to %s" % os.getcwd() + os.sep + out)
+    if save_txt or save_image:
+        print(f"Results saved to {os.getcwd() + os.sep + out}.")
 
-    print("Done. (%.3fs)" % (time.time() - t0))
+    print(f"Done. ({time.time() - start_time:.3f}s)")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--weights", type=str, default="weights/yolov5s.pth", help="model.pt path")
-    parser.add_argument("--source", type=str, default="inference/images", help="source")  # file/folder, 0 for webcam
-    parser.add_argument("--output", type=str, default="inference/output", help="output folder")  # output folder
-    parser.add_argument("--img-size", type=int, default=640, help="inference size (pixels)")
-    parser.add_argument("--conf-thres", type=float, default=0.4, help="object confidence threshold")
-    parser.add_argument("--iou-thres", type=float, default=0.5, help="IOU threshold for NMS")
-    parser.add_argument("--fourcc", type=str, default="mp4v", help="output video codec (verify ffmpeg support)")
+    parser.add_argument("--config-file", type=str, default="configs/COCO-Detection/yolov5s.yaml",
+                        help="Neural network profile path. (default: `configs/COCO-Detection/yolov5s.yaml`)")
+    parser.add_argument("--weights", type=str, default="weights/yolov5s.pth",
+                        help="Model file weight path. (default: `weights/yolov5s.pth`)")
+    parser.add_argument("--source", type=str, default="data/examples",
+                        help="Image input source. (default: `data/examples`)")
+    parser.add_argument("--output", type=str, default="output",
+                        help="Output result folder. (default: `output`)")
+    parser.add_argument("--image-size", type=int, default=640,
+                        help="Size of processing picture. (default: 640)")
+    parser.add_argument("--confidence-threshold", type=float, default=0.4,
+                        help="Object confidence threshold. (default=0.4)")
+    parser.add_argument("--iou-threshold", type=float, default=0.5,
+                        help="IOU threshold for NMS. (default=0.5)")
+    parser.add_argument("--fourcc", type=str, default="mp4v",
+                        help="output video codec (verify ffmpeg support). (default=mp4v)")
     parser.add_argument("--half", action="store_true", help="half precision FP16 inference")
-    parser.add_argument("--device", default="", help="cuda device, i.e. 0 or 0,1,2,3 or cpu")
-    parser.add_argument("--view-img", action="store_true", help="display results")
-    parser.add_argument("--save-txt", action="store_true", help="save results to *.txt")
-    parser.add_argument("--classes", nargs="+", type=int, help="filter by class")
-    parser.add_argument("--agnostic-nms", action="store_true", help="class-agnostic NMS")
-    parser.add_argument("--augment", action="store_true", help="augmented inference")
-    opt = parser.parse_args()
-    opt.img_size = check_img_size(opt.img_size)
-    print(opt)
+    parser.add_argument("--device", default="",
+                        help="device id (i.e. 0 or 0,1) or cpu. (default="")")
+    parser.add_argument("--view-image", action="store_true",
+                        help="Display results")
+    parser.add_argument("--save-txt", action="store_true",
+                        help="Save results to *.txt")
+    parser.add_argument("--classes", nargs="+", type=int,
+                        help="Filter by class")
+    parser.add_argument("--agnostic-nms", action="store_true",
+                        help="Class-agnostic NMS")
+    parser.add_argument("--augment", action="store_true",
+                        help="augmented inference")
+    args = parser.parse_args()
+    args.image_size = check_image_size(args.image_size)
+    print(args)
 
     with torch.no_grad():
         detect()
