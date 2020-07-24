da487ea4c403b11754a51fe43f792dc75c72c087
Liu Changyu <liuchangyu1111@gmail.com>
2020-07-24 23:19:50 +0800

    Add dataset location description

--- detect.py
+++ detect.py
@@ -169,7 +169,7 @@ def detect(save_img=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default='yolov5-small.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='assets/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--source', type=str, default='data/examples', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--output', type=str, default='outputs', help='output folder')  # output folder
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.4, help='object confidence threshold')
