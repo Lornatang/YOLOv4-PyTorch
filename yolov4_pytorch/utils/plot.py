0aa83de3a9d91b297694d03dee8ffb612eee370b
Liu Changyu <liuchangyu1111@gmail.com>
2020-07-26 12:33:41 +0800

    Improve the drawing method of the label box

--- yolov4_pytorch/utils/plot.py
+++ yolov4_pytorch/utils/plot.py
@@ -144,18 +144,20 @@ def plot_labels(labels, save_dir=''):
     plt.close()
 
 
-def plot_one_box(x, img, color=None, label=None, line_thickness=None):
-    # Plots one bounding box on image img
-    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+def plot_one_box(xyxy, image, color=None, label=None, line_thickness=None):
+    # Plots one bounding box on image
+    line_thickness = line_thickness or round(0.002 * (image.shape[0] + image.shape[1]) / 2) + 1
     color = color or [random.randint(0, 255) for _ in range(3)]
-    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    c1, c2 = (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3]))
+    cv2.rectangle(image, c1, c2, color, thickness=line_thickness, lineType=cv2.LINE_AA)
     if label:
-        tf = max(tl - 1, 1)  # font thickness
-        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        tf = max(line_thickness - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=line_thickness / 3, thickness=tf)[0]
         c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
-        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+        cv2.rectangle(image, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(image, label, (c1[0], c1[1] - 2), 0, line_thickness / 3, [225, 255, 255],
+                    thickness=tf,
+                    lineType=cv2.LINE_AA)
 
 
 def plot_results(start=0, stop=0, bucket='', id=(), labels=(),
