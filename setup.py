3fd682baf4c55ce700581c28f0f276091e755186
Liu Changyu <liuchangyu1111@gmail.com>
2020-07-29 13:14:56 +0800

    The code is updated to version 0.1.0, adding some basic weight information

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/Lornatang/YOLOv4-PyTorch'
 EMAIL = 'liuchangyu1111@gmail.com'
 AUTHOR = 'Liu Changyu'
 REQUIRES_PYTHON = '>=3.6.5'
-VERSION = '0.0.1'
+VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
